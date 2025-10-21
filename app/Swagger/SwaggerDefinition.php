9d67dcbd17b42476ba6e91918331728e85c270b1
arumanuevo <glipischeck@gmail.com>
2025-10-21 19:42:50 -0300

    correccion

--- app/Swagger/SwaggerDefinition.php
+++ app/Swagger/SwaggerDefinition.php
@@ -6,12 +6,15 @@ namespace App\Swagger;
  * @OA\Info(
  *     title="API del Marketplace",
  *     version="1.0.0",
- *     description="Documentación generada con L5-Swagger para la API del Marketplace.eeeeee"
- * )
- *
- * @OA\Server(
- *     url=L5_SWAGGER_CONST_HOST,
- *     description="Servidor principal"
+ *     description="Documentación generada con L5-Swagger para la API del Marketplace Orientado a la ciudad de Chivilcoy.",
+ *     @OA\Contact(
+ *         email="scastellano10@gmail.com",
+ *         name="Santiago Castellano"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
  * )
  */
 class SwaggerDefinition {}
