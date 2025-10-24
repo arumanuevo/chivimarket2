9886674c5aaebe6813aca642ca17263de879025f
arumanuevo <glipischeck@gmail.com>
2025-10-24 18:20:01 -0300

    cors 3000

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,14 +18,14 @@ class Cors
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         if ($request->isMethod('OPTIONS')) {
             return response('', 200)
-                ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000')
+                ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
                 ->header('Access-Control-Allow-Credentials', 'true');
