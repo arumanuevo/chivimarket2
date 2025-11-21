c86e67ba11a177425752d37c3e6617b6735f1fd4
arumanuevo <glipischeck@gmail.com>
2025-11-21 18:04:04 -0300

    actualizacion del cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8000', 'https://tudominio.com'],
+    'allowed_origins' => ['http://127.0.0.1:8000', 'https://tudominio.com', 'http://localhost/front_mensajeria/'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
