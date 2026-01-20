a28ba59529df61df9245c1c664fc0a6156a5c5f8
arumanuevo <glipischeck@gmail.com>
2026-01-20 19:42:09 -0300

    devolucion de datos de usuario enriquecido

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -1,7 +1,7 @@
 <?php
 
 // app/Http/Resources/UserResource.php
-namespace App\Http\Resources;
+/*namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -23,5 +23,57 @@ class UserResource extends JsonResource
             'subscription' => $this->subscription ? new SubscriptionResource($this->subscription) : null,
         ];
     }
+}*/
+
+// app/Http/Resources/UserResource.php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            // Datos de suscripción (si existe)
+            'subscription' => $this->subscription ? new SubscriptionResource($this->subscription) : null,
+
+            // Roles y permisos
+            'roles' => $this->roles,
+            'permissions' => $this->permissions,
+
+            // Negocios del usuario + conteo
+            'businesses' => $this->businesses,
+            'businesses_count' => $this->businesses->count(),  // <-- Conteo de negocios
+
+            // Datos adicionales para evaluaciones posteriores
+            'can_create_business' => $this->subscription ?
+                SubscriptionService::canCreateBusiness($this)['can_create'] :
+                false,
+
+            'can_create_product' => $this->subscription ?
+                SubscriptionService::canCreateProduct($this, $this->businesses->first()?->id)['can_create'] ?? false :
+                false,
+
+            // Límite de negocios según suscripción
+            'max_businesses_allowed' => $this->subscription ?
+                SubscriptionService::getMaxBusinessesForSubscription($this->subscription->type) :
+                1,  // Default para suscripción 'free'
+
+            // Límite de productos según suscripción
+            'max_products_allowed' => $this->subscription ?
+                SubscriptionService::getMaxProductsForSubscription($this->subscription->type) :
+                10  // Default para suscripción 'free'
+        ];
+    }
 }
+
 
