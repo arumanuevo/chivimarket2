86de0bca08ed0d755acf232b672918e8e67a7e75
arumanuevo <glipischeck@gmail.com>
2026-01-20 20:38:45 -0300

    correccion

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -30,6 +30,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Services\SubscriptionService;
 
 class UserResource extends JsonResource
 {
