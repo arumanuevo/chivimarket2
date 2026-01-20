78609623d823fa0731e320c6561b3bdbb35f7b37
arumanuevo <glipischeck@gmail.com>
2026-01-20 20:50:15 -0300

    dddd

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -30,6 +30,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Services\SubscriptionService;
 
 class UserResource extends JsonResource
 {
@@ -79,5 +80,3 @@ class UserResource extends JsonResource
         ];
     }
 }
-
-
