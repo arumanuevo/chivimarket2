8175572193dbdac20209396e09750df7b557e131
arumanuevo <glipischeck@gmail.com>
2026-02-05 16:58:44 -0300

    agregado de encabezados

--- app/Http/Controllers/API/ImageController.php
+++ app/Http/Controllers/API/ImageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller; // Importa la clase base Controller
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
@@ -16,8 +16,15 @@ class ImageController extends Controller
             abort(404);
         }
 
-        return response()->file($path);
+        $file = file_get_contents($path);
+        $response = response($file, 200);
+        $response->header('Content-Type', 'image/png'); // Ajusta el tipo de contenido según el tipo de imagen
+        $response->header('Cache-Control', 'public, max-age=31536000'); // Cachear la imagen por un año
+        $response->header('Access-Control-Allow-Origin', '*'); // Permitir solicitudes desde cualquier origen
+
+        return $response;
     }
 }
+
 
 
